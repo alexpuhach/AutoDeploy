9352b87f1f47fce688d5c87088d4e623ad75c010
Alex Puhach <spugac57@gmail.com>
2024-06-24 16:24:25 +0200

    selenium fix

--- tests/selenium_tests/test_selenium_example.py
+++ tests/selenium_tests/test_selenium_example.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 def test_google_search():
-    driver = webdriver.Firefox()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     driver.get("https://www.google.com")
     assert "Google" in driver.title
     driver.quit()
